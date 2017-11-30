9b31da1a2516c9d3d0571d2ca2636631f3dcdbdc
Vitjan Zavrtanik <vitjan.zavrtanik@gmail.com>
2017-11-30 16:03:44 +0100

    Docker build on travis, exceptionMapper, jackson serializer

---
+++ customers/customers-api/src/main/java/rso/project/customers/api/exception/Error.java
@@ -0,0 +1,31 @@
+package rso.project.customers.api.exception;
+
+public class Error {
+    private Integer status;
+    private String code;
+    private String message;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
