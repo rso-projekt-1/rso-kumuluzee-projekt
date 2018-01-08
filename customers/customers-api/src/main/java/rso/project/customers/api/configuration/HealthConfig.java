a2a98a038fe52edc01e276f57f3ed6a3bd0ae525
Vitjan Zavrtanik <vitjan.zavrtanik@gmail.com>
2018-01-08 22:48:33 +0100

    Health config test

---
+++ customers/customers-api/src/main/java/rso/project/customers/api/configuration/HealthConfig.java
@@ -0,0 +1,29 @@
+package rso.project.cdi.configuration;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.cdi.ConfigValue;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@ConfigBundle("rest-properties")
+public class RestProperties {
+
+    @ConfigValue(value = "external-dependencies.order-service.enabled",watch = true)
+    private boolean orderServiceEnabled;
+
+    @ConfigValue(value = "external-dependencies.order-service.fakeHealthy",watch = true)
+    private boolean orderServiceFakeHealthy;
+
+    public boolean isOrderServiceFakeHealthy(){ return orderServiceFakeHealthy; }
+    public void setOrderServiceFakeHealthy(boolean orderServiceFakeHealthy){
+        this.orderServiceFakeHealthy = orderServiceFakeHealthy;
+    }
+
+    public boolean isOrderServiceEnabled(){
+        return orderServiceEnabled;
+    }
+    public void setOrderServiceEnabled(boolean orderServiceEnabled){
+        this.orderServiceEnabled = orderServiceEnabled;
+    }
+}
