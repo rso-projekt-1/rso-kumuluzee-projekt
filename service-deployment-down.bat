1e0de479da5b456ef25b3025677549e5cbca4034
Vitjan Zavrtanik <vitjan.zavrtanik@gmail.com>
2018-01-18 20:20:25 +0100

    sql init script change

--- service-deployment-down.bat
+++ service-deployment-down.bat
@@ -5,10 +5,12 @@ kubectl get deployment
 kubectl delete -f .\kubernetes-config-files\order-service.yaml
 kubectl delete -f .\kubernetes-config-files\customer-service.yaml
 kubectl delete -f .\kubernetes-config-files\video-service.yaml
+kubectl delete -f .\kubernetes-config-files\playlists-service.yaml
 
 kubectl delete -f .\kubernetes-config-files\order-deployment.yaml
 kubectl delete -f .\kubernetes-config-files\customer-deployment.yaml
 kubectl delete -f .\kubernetes-config-files\video-deployment.yaml
+kubectl delete -f .\kubernetes-config-files\playlists-deployment.yaml
 
 
 kubectl get svc
