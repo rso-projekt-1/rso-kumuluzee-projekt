e49e099e644606c7b24fd722a1e86a526235a317
Vitjan Zavrtanik <vitjan.zavrtanik@gmail.com>
2018-01-19 03:11:54 +0100

    recommender fix

--- recommender/src/main/java/RecommenderResource.java
+++ recommender/src/main/java/RecommenderResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
-@ApplicationPath("recommender")
+@Path("recommender")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class RecommenderResource {
